fd5c060556339eb69a5f752cdda51d55389a2970
Andy Liu <57054829+AndyLiu314@users.noreply.github.com>
2026-01-16 00:46:41 -0800

    Defined custom window class

--- src/WinMain.cpp
+++ src/WinMain.cpp
@@ -1,65 +1,4 @@
-#include "win/CustomWin.h"
-#include "win/WindowsMessageMap.h"
-#include <string>
-#include <iostream>
-#include <sstream>
-
-// Helper function to convert std::string to std::wstring
-inline std::wstring StringToWString(const std::string& str)
-{
-	int size_needed = MultiByteToWideChar(CP_UTF8, 0, str.c_str(), (int)str.size(), nullptr, 0);
-	std::wstring wstr(size_needed, 0);
-	MultiByteToWideChar(CP_UTF8, 0, str.c_str(), (int)str.size(), &wstr[0], size_needed);
-	return wstr;
-}
-
-LRESULT CALLBACK WndProc(
-	_In_ HWND   hWnd,
-	_In_ UINT   msg,
-	_In_ WPARAM wParam,
-	_In_ LPARAM lParam)
-{
-	static WindowsMessageMap messageMap;
-	OutputDebugStringW(StringToWString(messageMap(msg, lParam, wParam)).c_str());
-
-	switch (msg) {
-	case WM_CLOSE:
-		PostQuitMessage(0);
-		break;
-
-	case WM_KEYDOWN:
-		if (wParam == 'F') {
-			SetWindowText(hWnd, L"The 'F' key was pressed!");
-		}
-		break;
-
-	case WM_KEYUP:
-		if (wParam == 'F') {
-			SetWindowText(hWnd, L"released F");
-		}
-		break;
-
-	case WM_CHAR:
-		{
-			static std::wstring title;
-			title.push_back(static_cast<wchar_t>(wParam));
-			SetWindowText(hWnd, title.c_str());
-		
-		}
-		break;
-
-	case WM_LBUTTONDOWN:
-		{
-			POINTS pt = MAKEPOINTS(lParam);
-			std::wostringstream oss;
-			oss << L"Mouse Clicked at (" << pt.x << L"," << pt.y << L")";
-			SetWindowText(hWnd, oss.str().c_str());
-		}
-		break;
-	}
-
-	return DefWindowProc(hWnd, msg, wParam, lParam);
-}
+#include "win/Window.h"
 
 int CALLBACK WinMain(
 	_In_ HINSTANCE hInstance,
@@ -67,44 +6,20 @@ int CALLBACK WinMain(
 	_In_ LPSTR     lpCmdLine,
 	_In_ int       nCmdShow)
 {
-	const auto pClassName = L"EngineD3D";
-
-	WNDCLASSEX wc = { 0 };
-	wc.cbSize = sizeof(wc);
-	wc.style = CS_OWNDC;
-	wc.lpfnWndProc = WndProc;
-	wc.cbClsExtra = 0;
-	wc.cbWndExtra = 0;
-	wc.hInstance = hInstance;
-	wc.hIcon = nullptr;
-	wc.hIconSm = nullptr;
-	wc.hCursor = nullptr;
-	wc.hbrBackground = nullptr;
-	wc.lpszMenuName = nullptr;
-	wc.lpszClassName = pClassName;
-	RegisterClassEx(&wc);
-
-	HWND hWnd = CreateWindowEx(
-		0, pClassName,
-		L"Fundamental Direct3D 11",
-		WS_CAPTION | WS_MINIMIZEBOX | WS_SYSMENU,
-		200, 200, 640, 480,
-		nullptr, nullptr, hInstance, nullptr
-	);
-
-	ShowWindow(hWnd, SW_SHOW);
+	Window wnd(800, 300, L"Donkey Fart Box");
 
 	MSG msg;
-	BOOL resultCode;
-	while ((resultCode = GetMessage(&msg, nullptr, 0, 0)) > 0) {
+	BOOL gResult;
+	while ((gResult = GetMessage(&msg, nullptr, 0, 0)) > 0)
+	{
 		TranslateMessage(&msg);
 		DispatchMessage(&msg);
 	}
 
-	if (resultCode == -1) {
+	if (gResult == -1)
+	{
 		return -1;
 	}
-	else {
-		return msg.wParam;
-	}
+
+	return static_cast<int>(msg.wParam);
 }
