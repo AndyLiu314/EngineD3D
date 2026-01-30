1efb83254d22b1aa6802c7eec60c082aa4295d8d
Andy Liu <57054829+AndyLiu314@users.noreply.github.com>
2026-01-30 14:48:11 -0800

    Added graphics exception class for error handling

--- include/core/Graphics.h
+++ include/core/Graphics.h
@@ -1,9 +1,33 @@
 #pragma once
 #include "win/CustomWin.h"
+#include "core/EngineException.h"
 #include <d3d11.h>
 
 class Graphics 
 {
+public:
+	class Exception : public EngineException
+	{
+	public:
+		Exception(int line, const char* file, HRESULT hr) noexcept;
+
+		const char* what() const noexcept override;
+		virtual const char* GetType() const noexcept override;
+		HRESULT GetErrorCode() const noexcept;
+		static std::string TranslateErrorCode(HRESULT hr) noexcept;
+		std::string GetErrorString() const noexcept;
+
+	private:
+		HRESULT hr;
+	};
+
+	class DeviceRemovedException : public Exception
+	{
+	public:
+		using Exception::Exception;
+		const char* GetType() const noexcept override;
+	};
+
 public:
 	Graphics(HWND hWnd);
 	Graphics(const Graphics&) = delete;
@@ -19,3 +43,7 @@ private:
 	ID3D11DeviceContext* pContext = nullptr;
 	ID3D11RenderTargetView* pTarget = nullptr;
 };
+
+#define EGFX_EXCEPT(hr) Graphics::Exception(__LINE__, __FILE__, hr)
+#define EGFX_THROW_FAILED(hrcall) if (FAILED(hr = (hrcall))) throw EGFX_EXCEPT(hr)
+#define EGFX_DEVICE_REMOVED_EXCEPT(hr) Graphics::DeviceRemovedException(__LINE__, __FILE__, hr)
--- src/core/Graphics.cpp
+++ src/core/Graphics.cpp
@@ -1,7 +1,73 @@
 #include "core/Graphics.h"
+#include "utility/StringConversion.h"
+#include <sstream>
 
 #pragma comment(lib, "d3d11.lib")
 
+
+/* --------- GRAPHICS EXCEPTION --------- */
+Graphics::Exception::Exception(int line, const char* file, HRESULT hr) noexcept
+	: EngineException(line, file), hr(hr)
+{
+}
+
+const char* Graphics::Exception::what() const noexcept
+{
+	std::ostringstream oss;
+	oss << GetType() << std::endl
+		<< "[Error Code] " << GetErrorCode() << std::endl
+		<< "[Description] " << GetErrorString() << std::endl
+		<< GetOriginString();
+	whatBuffer = oss.str();
+	return whatBuffer.c_str();
+}
+
+const char* Graphics::Exception::GetType() const noexcept
+{
+	return "Engine Graphics Exception";
+}
+
+std::string Graphics::Exception::TranslateErrorCode(HRESULT hr) noexcept
+{
+	wchar_t* pMsgBuf = nullptr;
+	DWORD nMsgLen = FormatMessage(
+		FORMAT_MESSAGE_ALLOCATE_BUFFER | FORMAT_MESSAGE_FROM_SYSTEM | FORMAT_MESSAGE_IGNORE_INSERTS,
+		nullptr,
+		hr,
+		MAKELANGID(LANG_NEUTRAL, SUBLANG_DEFAULT),
+		reinterpret_cast<LPWSTR>(&pMsgBuf),
+		0,
+		nullptr
+	);
+
+	if (nMsgLen == 0)
+	{
+		return "Unidentified Error Code";
+	}
+
+	std::string errorString = ToNarrow(pMsgBuf);
+	LocalFree(pMsgBuf);
+	return errorString;
+}
+
+HRESULT Graphics::Exception::GetErrorCode() const noexcept
+{
+	return hr;
+}
+
+std::string Graphics::Exception::GetErrorString() const noexcept
+{
+	return TranslateErrorCode(hr);
+}
+
+const char* Graphics::DeviceRemovedException::GetType() const noexcept
+{
+	return "Engine Graphics Exception [Device Removed] (DXGI_ERROR_DEVICE_REMOVED)";
+}
+/* --------- GRAPHICS EXCEPTION --------- */
+
+
+/* --------- GRAPHICS --------- */
 Graphics::Graphics(HWND hWnd)
 {
 	DXGI_SWAP_CHAIN_DESC sd = {};
@@ -21,11 +87,18 @@ Graphics::Graphics(HWND hWnd)
 	sd.SwapEffect = DXGI_SWAP_EFFECT_DISCARD;
 	sd.Flags = 0;
 
-	D3D11CreateDeviceAndSwapChain(
+	UINT flags = 0;
+	#ifdef _DEBUG
+	flags |= D3D11_CREATE_DEVICE_DEBUG;
+	#endif
+
+	HRESULT hr;
+
+	EGFX_THROW_FAILED(D3D11CreateDeviceAndSwapChain(
 		nullptr,
 		D3D_DRIVER_TYPE_HARDWARE,
 		nullptr,
-		0,
+		flags,
 		nullptr,
 		0,
 		D3D11_SDK_VERSION,
@@ -34,19 +107,22 @@ Graphics::Graphics(HWND hWnd)
 		&pDevice,
 		nullptr,
 		&pContext
-	);
+	));
 
 	ID3D11Resource* pBackBuffer = nullptr;
-	pSwapChain->GetBuffer(
+
+	EGFX_THROW_FAILED(pSwapChain->GetBuffer(
 		0,
 		__uuidof(ID3D11Resource),
 		reinterpret_cast<void**>(&pBackBuffer)
-	);
-	pDevice->CreateRenderTargetView(
+	));
+
+	EGFX_THROW_FAILED(pDevice->CreateRenderTargetView(
 		pBackBuffer,
 		nullptr,
 		&pTarget
-	);
+	));
+
 	pBackBuffer->Release();
 }
 
@@ -72,7 +148,18 @@ Graphics::~Graphics()
 
 void Graphics::EndFrame()
 {
-	pSwapChain->Present(1u, 0u);
+	HRESULT hr;
+	if (FAILED(hr = pSwapChain->Present(1u, 0u)))
+	{
+		if (hr == DXGI_ERROR_DEVICE_REMOVED)
+		{
+			throw EGFX_DEVICE_REMOVED_EXCEPT(pDevice->GetDeviceRemovedReason());
+		}
+		else
+		{
+			EGFX_THROW_FAILED(hr);
+		}
+	}
 }
 
 void Graphics::ClearBuffer(float r, float g, float b) noexcept
@@ -80,3 +167,4 @@ void Graphics::ClearBuffer(float r, float g, float b) noexcept
 	const float colour[] = { r, g, b, 1.0f };
 	pContext->ClearRenderTargetView(pTarget, colour);
 }
+/* --------- GRAPHICS --------- */
